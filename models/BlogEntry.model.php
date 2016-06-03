47351fd49d182126096bfc69fcf6687d2aba2c2a
Michael Heath <m.heath66@gmail.com>
2016-06-02 23:57:00 -0300

    Formatting

--- models/BlogEntry.model.php
+++ models/BlogEntry.model.php
@@ -2,21 +2,18 @@
 
 namespace SimpleBlog;
 
-class BlogEntryModel extends \Sifo\Model
-{
+class BlogEntryModel extends \Sifo\Model{
     
-    public function getBlogEntries()
-    {
+    public function getBlogEntries(){
         return $this->GetArray('SELECT * from blog_entries', array('tag' => 'Get all entries'));
     }
     
-    public function addBlogEntry($title, $content)
-    {
+    public function addBlogEntry($title, $content){
         $sql = "insert into blog_entries (title, content) VALUES (?,?)";
         $result = $this->Execute( $sql, array(
-                                    'tag' => 'Add message',
-                                    $title,
-                                    $content
-		                             ) );
+                                  'tag' => 'Add message',
+                                  $title,
+                                  $content
+		                  ) );
     }
 }

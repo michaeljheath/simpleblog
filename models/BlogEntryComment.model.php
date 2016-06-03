9359ee6c9ed6a652718200d7c5c04e59a90698af
Michael Heath <m.heath66@gmail.com>
2016-06-02 23:58:26 -0300

    Formatting

--- models/BlogEntryComment.model.php
+++ models/BlogEntryComment.model.php
@@ -2,11 +2,9 @@
 
 namespace SimpleBlog;
 
-class BlogEntryCommentModel extends \Sifo\Model
-{
+class BlogEntryCommentModel extends \Sifo\Model{
     
-    public function getBlogEntryComments($blogEntryId)
-    {
+    public function getBlogEntryComments($blogEntryId){
         $sql = 'select comments.comment from comments, blog_entries, blog_entry_comments 
                 where comments.id = blog_entry_comments.comment_id
                 and blog_entry_comments.blog_entry_id = blog_entries.id
@@ -15,24 +13,22 @@ class BlogEntryCommentModel extends \Sifo\Model
         return $this->GetArray($sql, array($blogEntryId));
     }
     
-    public function addBlogEntryComment($blogEntryId, $comment)
-    {
+    public function addBlogEntryComment($blogEntryId, $comment){
         
         $sql = "insert into comments (comment) VALUES (?)";
         $result = $this->Execute( $sql, array(
-                                    'tag' => 'Add message',
-                                    $comment
-		                             ) );
+                                  'tag' => 'Add message',
+                                  $comment
+		                 ));
         
         // get the latest comment_id
         $commentId = $this->Insert_ID();
-        if($result)
-        {
+        if($result){
             $sql = "insert into blog_entry_comments (blog_entry_id, comment_id) VALUES (?,?)";
             $result = $this->Execute($sql, array(
-                                        'tag' => 'Add message',
-                                        $blogEntryId,
-                                        $commentId
+                                     'tag' => 'Add message',
+                                     $blogEntryId,
+                                     $commentId
                                     ));
             
             if($result)
@@ -42,6 +38,4 @@ class BlogEntryCommentModel extends \Sifo\Model
             
         }
     }
-
-
 }
